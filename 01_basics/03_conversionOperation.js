3699e7f1550f495e94fee9a82d6face4c1c0a3d3
Shivangi Shrivastava <147822546+shivi1100@users.noreply.github.com>
2023-10-14 06:18:40 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,41 @@ let valueInNumber = Number(score);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+ //  ****************************  Operations  *********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2/2)
+//console.log(2%2)
+
+let str1 = "hello"
+let str2 = " shivangi"
+let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1"+ 2 + 2) // if first is string that whole number convert into string
+console.log(1 + 2 + "2") // if first digit in numbers that whole convert into inumber and then string
+
+console.log((3 + 4) * 5 % 3)
+
+console.log(+true) // not preferable
+console.log(+"") // not preferable
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1) // for comparision data types of both operator should be same
+
+// console.log(null > 0)
+// console.log(null >= 0)
+// console.log(null == 0)
+//=================== avoid this type of null and undefined comparision==================
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+// === strict-check
+
+console.log("2" === 2) // also compare data types
