5cac3dbc912ed97979a1947828ac07cca060dc84
Shivangi Shrivastava <147822546+shivi1100@users.noreply.github.com>
2023-10-15 05:33:47 +0000

    num_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+const score = 400;
+// console.log(score)
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8996
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')) 
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5))
+// console.log(Math.round(7.6))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(81))
+// console.log(Math.min(4, 5, 6, 7))
+// console.log(Math.max(4, 5, 6, 7))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log(Math.floor((Math.random()*10) + 1))
+
+const min = 15;
+const max = 25;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
