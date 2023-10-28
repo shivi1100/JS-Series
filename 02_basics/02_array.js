1d0fe91f12ea3d3ea5dc082718954537650b5c7d
Shivangi Shrivastava <147822546+shivi1100@users.noreply.github.com>
2023-10-28 11:02:47 +0000

    array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heroes = ["thor","Ironman","spiderman",]
+const dc_heroes = ["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes)
+// console.log(marvel_heroes[3][2])
+
+const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes)
+
+const all_new_heros = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heros)
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
+
+console.log(Array.isArray("Shivi")) // how to convert it into array?
+console.log(Array.from("Shivi")) 
+console.log(Array.from({name:"shivi"})) //interesting** 
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+console.log(Array.of(score1, score2, score3))
+
